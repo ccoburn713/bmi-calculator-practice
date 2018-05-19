999843f3f98e3298635ffe182323bedc7208b181
Christian Coburn <ccoburn713@Gmail.com>
2018-05-19 03:44:05 -0700

    Added files

---
+++ bmi.js
@@ -0,0 +1,20 @@
+
+var valid = function(){
+    var weight = document.getElementById('weight').value;
+    var height = document.getElementById('height').value;
+
+
+    if(isNaN(weight || height)){
+        return alert("Value must be a number!");
+    }
+    if(weight || height === "" ){
+        return alert("Please enter a value")
+    }
+    else{
+        var result = ((weight / (Math.pow(height, 2))) * 703);
+        var result = parseFloat(result).toFixed(2)
+        // return alert("Your BMI is " + result)
+        return document.getElementById('resultline').innerHTML=("Your BMI is " + result);
+    }
+}
+
