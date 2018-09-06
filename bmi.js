7f0693690e5db46c66e832aad9b8ea48dbca9df3
Christian Coburn <ccoburn713@gmail.com>
2018-09-06 03:43:44 -0700

    fix errors

--- bmi.js
+++ bmi.js
@@ -1,20 +1,20 @@
 
-var valid = function(){
+var valid = function () {
     var weight = document.getElementById('weight').value;
     var height = document.getElementById('height').value;
 
 
-    if(isNaN(weight || height)){
+    if (isNaN(weight) || isNaN(height)) {
         return alert("Value must be a number!");
     }
-    if(weight || height === "" ){
-        return alert("Please enter a value")
+    if (weight === "" || height === "") {
+        return alert("Please enter a value");
     }
-    else{
+    else {
         var result = ((weight / (Math.pow(height, 2))) * 703);
         var result = parseFloat(result).toFixed(2)
         // return alert("Your BMI is " + result)
-        return document.getElementById('resultline').innerHTML=("Your BMI is " + result);
+        return document.getElementById('resultline').innerHTML = ("Your BMI is " + result);
     }
 }
 
